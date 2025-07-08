4a67f64629f292f27ffbbe591690ad3f116784c9
Talles Souza <tallessouza117@gmail.com>
2025-07-07 21:16:33 -0300

    Menu do jogo

---
+++ main/JogoDaVelha.java
@@ -0,0 +1,35 @@
+package main;
+
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Scanner;
+
+public class JogoDaVelha {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        do {
+            System.out.println("1 _ 1v1");
+            System.out.println("2 _ computador");
+            System.out.println("3 _ modo diiferente");
+            System.out.println("4 _ ");
+            System.out.println("5 _ credito");
+
+            System.out.println("Opção: ");
+            int opcao = scanner.nextInt();
+
+            switch (opcao) {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+            }
+
+        } while (true);
+    }
+}
